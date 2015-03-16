8737a2fd5a0755152c8cb526ac5d71f9ca353fda
thaeliss <libj@kth.se>
2015-03-16 13:25:56 +0100

    Create home_view.js

---
+++ js/view/home_view.js
@@ -0,0 +1,10 @@
+//ExampleView Object constructor
+var HomeView = function (container, modell) {
+	
+	// Get all the relevant elements of the view (ones that show data
+  	// and/or ones that responed to interaction)
+	//modell.addObserver(this);
+	this.createButton = container.find("#newDinner");
+	
+}
+ 
