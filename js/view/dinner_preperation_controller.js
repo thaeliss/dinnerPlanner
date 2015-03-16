0167e679946f3b9402fc4feaa6fa0e39987d0c9e
thaeliss <libj@kth.se>
2015-03-16 13:23:36 +0100

    Create dinner_preperation_controller.js

---
+++ js/view/dinner_preperation_controller.js
@@ -0,0 +1,11 @@
+//ExampleView Object constructor
+var DinnerPrepCtrl = function (view, modell) {
+	
+	
+	view.editdinnerButton.click(function()
+	{
+		gotos.gotoSelectDishpage(); // ÄNDRA
+	})
+		
+}
+ 
